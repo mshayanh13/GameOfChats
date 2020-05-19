4b3a6166b34a9048dbc6a6973004dc5d065a666a
Mohammad Shayan <mohammadhemani@gmail.com>
2020-05-19 15:47:21 +0430

    Initial Commit

---
+++ GameOfChats/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameOfChats
+//
+//  Created by Mohammad Shayan on 5/19/20.
+//  Copyright © 2020 Mohammad Shayan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
